a8f60ce44b3bb0976bd665b4efb90c8b6be3b7f2
tetzng <ffffffffffffunk@gmail.com>
2020-03-05 14:37:05 +0900

    終了した部屋一覧を表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,5 +3,6 @@
 class RoomsController < ApplicationController
   def index
     @unstarted_rooms = Room.where('start_time > ?', Time.zone.now)
+    @ended_rooms = Room.where('end_time < ?', Time.zone.now)
   end
 end
