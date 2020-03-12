b5ca3b6634453cde69719049151137aaea239a1c
tetzng <ffffffffffffunk@gmail.com>
2020-03-12 18:19:11 +0900

    同じ部屋内でチャットができる

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'rooms#index'
   resources :rooms do
     get 'join', on: :member
+    resources :messages, only: %i[index create]
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
