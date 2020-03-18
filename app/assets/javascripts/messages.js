72171a3d6f6b6f9916c88e2986e837b0ee101b56
tetzng <ffffffffffffunk@gmail.com>
2020-03-18 14:43:45 +0900

    自動リロード処理viewに移行

--- app/assets/javascripts/messages.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('turbolinks:load',function(){
-  const path = location.pathname ;
-  const interval = 60000;
-
-  if ( path.match(/message/)) {
-    setInterval('location.reload()',interval);
-  }
-});
